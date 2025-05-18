82a69b568dacadbc8cb6a4449606a5b3eb6f5043
Dariusgrey <darioalvarezram@gmail.com>
2025-05-18 16:17:07 -0600

    Variables

---
+++ src/Main/Variables.kt
@@ -0,0 +1,13 @@
+package Main
+
+fun main(){
+    var nombre = "Dario"
+    println("Nombre: $nombre")
+
+    nombre = "Luis"
+
+    println("Nombre modificado: $nombre")
+
+    var edad = 29
+    var altura = 1.73
+}
