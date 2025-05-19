57d1cff063588547ad8b17bdb2941e97ea5dc65f
Dariusgrey <darioalvarezram@gmail.com>
2025-05-18 19:35:06 -0600

    uso de clase Scanner de Java en Kotlin

--- src/Main/ReadLine.kt
+++ src/Main/ReadLine.kt
@@ -1,5 +1,7 @@
 package Main
 
+import java.util.*
+
 fun main(){
     println("Dame tu nombre:")
     val nombre = readln()
@@ -12,4 +14,25 @@ fun main(){
     println("Dame tu altura:")
     val altura = readln().toFloat()
     println("Edad: $altura")
+
+// Alternativa usando Java Scanner
+    val scanner = Scanner(System.`in`)
+
+    // Leer string
+    println("Introduce un texto:")
+    val texto = scanner.nextLine()
+    println("Texto ingresado $texto")
+
+    // Leer un entero
+    println("Introduce un numero entero:")
+    val numeroEntero = scanner.nextInt()
+    println("Numero entero ingresado $numeroEntero")
+
+    // Leer un entero
+    println("Introduce un numero flotante/decimal:")
+    val numeroFlotante = scanner.nextFloat()
+    println("Numero flotante ingresado $numeroFlotante")
 }
+
+
+
